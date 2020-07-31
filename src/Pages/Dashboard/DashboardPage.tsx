c196a2602d7e191202436414d85c815c282c75ed
Babi Hannah <barbara.teixeira@taqtile.com.br>
2020-07-31 11:22:38 -0300

    Add dashboard page

---
+++ src/Pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { AUTH_TOKEN } from "src/constants"
+import { RouteComponentProps } from "react-router"
+
+class DashboardPage extends React.Component<RouteComponentProps> {
+  render(): JSX.Element {
+    return (
+      <section>
+        <h1>Seja bem vindo a TaqTile!</h1>
+        <img src="https://http.cat/200"></img>
+        <button onClick={this.logout}>SAIR</button>
+      </section>
+    )
+  }
+
+  private logout = () => {
+    localStorage.removeItem(AUTH_TOKEN)
+    this.props.history.push("/login")
+  }
+}
+
+export { DashboardPage }
