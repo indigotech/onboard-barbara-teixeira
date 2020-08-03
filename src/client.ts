0a7661793d4f0e6cfe92c50a4315302a5277b6be
Babi Hannah <barbara.teixeira@taqtile.com.br>
2020-08-03 18:23:27 -0300

    Add token validation on client component

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,22 @@
-import { InMemoryCache, ApolloClient } from "@apollo/client"
+import { InMemoryCache, ApolloClient, createHttpLink } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
+import { AUTH_TOKEN } from "src/constants"
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: `${token}`,
+    },
+  }
+})
 
 const clientUrl = "https://tq-template-server-sample.herokuapp.com/graphql"
+const httpLink = createHttpLink({ uri: clientUrl })
 
 const client = new ApolloClient({
-  uri: clientUrl,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
